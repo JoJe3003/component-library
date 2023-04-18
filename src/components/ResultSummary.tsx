54b5f58538738189905d5f1002fa08fe612f2278
JoJe3003 <jfjespersen@gmail.com>
2023-04-18 23:48:04 +0200

    Added Prettier and Prettier-Plugin-Tailwindcss and parsed every file

--- src/components/ResultSummary.tsx
+++ src/components/ResultSummary.tsx
@@ -1,112 +1,115 @@
-import { Noto_Sans } from 'next/font/google';
+import { Noto_Sans } from "next/font/google";
 
 const mainFont = Noto_Sans({
-	subsets: ['latin'],
-	weight: '400',
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export default function ResultSummary() {
-	return (
-		<div className={mainFont.className}>
-			<div className="flex flex-col m-4 text-white bg-white shadow-lg sm:flex-row rounded-3xl">
-				<div className="flex flex-col items-center justify-center gap-8 p-8 sm:gap-10 sm:max-w-xs min-w-xs bg-gradient-to-t from-blue-500 to-sky-600 rounded-3xl">
-					<h1 className="text-2xl font-semibold text-sky-50">Your Result</h1>
-					<p className="flex flex-col items-center justify-center w-56 h-56 gap-2 text-xl text-gray-400 rounded-full sm:w-48 sm:h-48 bg-gradient-to-t from-transparent to-blue-950/40 sm:text-base">
-						<span className="font-bold text-white text-8xl sm:text-7xl">76</span> of 100
-					</p>
-					<p className="flex flex-col items-center gap-2 text-sm text-center">
-						<span className="text-xl font-semibold">Great</span>
-						<span>
-							You scored higher than <span>65%</span> of the people who have taken
-							these tests.
-						</span>
-					</p>
-				</div>
-				<div className="flex flex-col gap-6 p-8 sm:gap-8 text-gray-950">
-					<h2 className="text-xl font-semibold">Summary</h2>
-					<div className="flex flex-col gap-2 min-w-[16rem]">
-						<div className="flex justify-between p-2 rounded-md sm:p-4 bg-gradient-to-r from-red-500/10 to-red-200/5">
-							<div className="flex items-center gap-2 font-bold text-red-600">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="18"
-									height="18"
-									viewBox="0 0 16 16"
-								>
-									<path
-										fill="currentColor"
-										d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41L4.157 8.5z"
-									/>
-								</svg>
-								Reaction
-							</div>
-							<div>
-								<span className="font-bold">80</span> / 100
-							</div>
-						</div>
-						<div className="flex justify-between p-2 rounded-md sm:p-4 bg-yellow-500/10">
-							<div className="flex items-center gap-2 font-bold text-yellow-600">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="18"
-									height="18"
-									viewBox="0 0 28 28"
-								>
-									<path
-										fill="currentColor"
-										d="M13 4c-1.91 0-3.457 1.387-3.844 3.188C6.824 7.604 5 9.55 5 12c0 .246.027.465.063.688C4.418 13.636 4 14.753 4 16c0 1.797.895 3.309 2.156 4.406c-.09.356-.156.711-.156 1.094c0 2.473 2.027 4.5 4.5 4.5h.063c.582 1.129 1.59 2 2.937 2a3.49 3.49 0 0 0 2.5-1.063A3.49 3.49 0 0 0 18.5 28c1.348 0 2.355-.871 2.938-2h.062c2.348 0 4.273-1.828 4.469-4.125H26c0-.063-.027-.125-.031-.188c.004-.062.031-.125.031-.187c0-.371-.133-.688-.219-1.031C27.078 19.367 28 17.829 28 16c0-1.246-.418-2.363-1.063-3.313c.036-.222.063-.44.063-.687c0-2.45-1.824-4.395-4.156-4.813C22.457 5.388 20.91 4 19 4c-1.2 0-2.266.559-3 1.406C15.266 4.56 14.2 4 13 4zm0 2c1.117 0 2 .883 2 2v2c0 1.668-1.332 3-3 3v2c1.133 0 2.16-.39 3-1.031V24.5c0 .84-.66 1.5-1.5 1.5a1.47 1.47 0 0 1-1.469-1.219l-.187-.968l-.969.156A2.487 2.487 0 0 1 8 21.5c0-.309.05-.598.156-.875c.008-.023.024-.04.031-.063a2.468 2.468 0 0 1 1.813-1.5l-.406-1.968A4.452 4.452 0 0 0 7 18.656A3.958 3.958 0 0 1 6 16a3.9 3.9 0 0 1 .875-2.469l.313-.375l-.094-.5A2.858 2.858 0 0 1 7 12c0-1.668 1.332-3 3-3h1V8c0-1.117.883-2 2-2zm6 0c1.117 0 2 .883 2 2v1h1c1.668 0 3 1.332 3 3c0 .215-.04.422-.094.656l-.093.5l.312.375A3.9 3.9 0 0 1 26 16c0 1.2-.512 2.27-1.344 3c-.683-.605-1.554-1-2.531-1c-.945 0-1.797.336-2.469.906l1.282 1.532A1.806 1.806 0 0 1 22.125 20c.965 0 1.734.723 1.844 1.656C23.887 22.973 22.836 24 21.5 24c-.09 0-.2-.004-.375-.031l-.969-.157l-.187.97A1.47 1.47 0 0 1 18.5 26c-.84 0-1.5-.66-1.5-1.5V13.969A4.915 4.915 0 0 0 20 15v-2c-1.668 0-3-1.332-3-3V8c0-1.117.883-2 2-2z"
-									/>
-								</svg>
-								Memory
-							</div>
-							<div>
-								<span className="font-bold">92</span> / 100
-							</div>
-						</div>
-						<div className="flex justify-between p-2 rounded-md sm:p-4 bg-green-500/10">
-							<div className="flex items-center gap-2 font-bold text-green-600">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="16"
-									height="16"
-									viewBox="0 0 22 22"
-								>
-									<path
-										fill="currentColor"
-										d="M12 15q.325 0 .537-.213t.213-.537v-8.5q0-.325-.213-.537T12 5q-.325 0-.537.213t-.213.537v8.5q0 .325.213.537T12 15Zm-2.75-2q.325 0 .537-.213T10 12.25v-4.5q0-.325-.213-.537T9.25 7q-.325 0-.537.213T8.5 7.75v4.5q0 .325.213.537T9.25 13Zm-2.5-2q.325 0 .537-.213t.213-.537v-.5q0-.325-.213-.537T6.75 9q-.325 0-.537.213T6 9.75v.5q0 .325.213.537T6.75 11Zm8 2q.325 0 .537-.213t.213-.537v-4.5q0-.325-.213-.537T14.75 7q-.325 0-.537.213T14 7.75v4.5q0 .325.213.537t.537.213Zm2.5-2q.325 0 .537-.213T18 10.25v-.5q0-.325-.213-.537T17.25 9q-.325 0-.537.213t-.213.537v.5q0 .325.213.537t.537.213ZM2 19.575V4q0-.825.588-1.413T4 2h16q.825 0 1.413.588T22 4v12q0 .825-.588 1.413T20 18H6l-2.3 2.3q-.475.475-1.088.213T2 19.575Zm2-2.4L5.175 16H20V4H4v13.175ZM4 4v13.175V4Z"
-									/>
-								</svg>
-								Verbal
-							</div>
-							<div>
-								<span className="font-bold">61</span> / 100
-							</div>
-						</div>
-						<div className="flex justify-between p-2 rounded-md sm:p-4 bg-violet-500/10">
-							<div className="flex items-center gap-2 font-bold text-violet-600">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									width="18"
-									height="18"
-									viewBox="0 0 24 24"
-								>
-									<path
-										fill="currentColor"
-										d="M12 6.5a9.77 9.77 0 0 1 8.82 5.5c-1.65 3.37-5.02 5.5-8.82 5.5S4.83 15.37 3.18 12A9.77 9.77 0 0 1 12 6.5m0-2C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 5a2.5 2.5 0 0 1 0 5a2.5 2.5 0 0 1 0-5m0-2c-2.48 0-4.5 2.02-4.5 4.5s2.02 4.5 4.5 4.5s4.5-2.02 4.5-4.5s-2.02-4.5-4.5-4.5z"
-									/>
-								</svg>
-								Visual
-							</div>
-							<div>
-								<span className="font-bold">72</span> / 100
-							</div>
-						</div>
-					</div>
-					<button className="p-2 text-white transition duration-75 rounded-full bg-sky-700 hover:bg-sky-800 hover:shadow-md active:translate-y-0.5">
-						Continue
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={mainFont.className}>
+      <div className="m-4 flex flex-col rounded-3xl bg-white text-white shadow-lg sm:flex-row">
+        <div className="min-w-xs flex flex-col items-center justify-center gap-8 rounded-3xl bg-gradient-to-t from-blue-500 to-sky-600 p-8 sm:max-w-xs sm:gap-10">
+          <h1 className="text-2xl font-semibold text-sky-50">Your Result</h1>
+          <p className="flex h-56 w-56 flex-col items-center justify-center gap-2 rounded-full bg-gradient-to-t from-transparent to-blue-950/40 text-xl text-gray-400 sm:h-48 sm:w-48 sm:text-base">
+            <span className="text-8xl font-bold text-white sm:text-7xl">
+              76
+            </span>
+            of 100
+          </p>
+          <p className="flex flex-col items-center gap-2 text-center text-sm">
+            <span className="text-xl font-semibold">Great</span>
+            <span>
+              You scored higher than <span>65%</span> of the people who have
+              taken these tests.
+            </span>
+          </p>
+        </div>
+        <div className="flex flex-col gap-6  p-8 text-gray-950 sm:gap-8">
+          <h2 className="text-xl font-semibold">Summary</h2>
+          <div className="flex min-w-[16rem] flex-col gap-2">
+            <div className="flex justify-between rounded-md bg-gradient-to-r from-red-500/10 to-red-200/5 p-2 sm:p-4">
+              <div className="flex items-center gap-2 font-bold text-red-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41L4.157 8.5z"
+                  />
+                </svg>
+                Reaction
+              </div>
+              <div>
+                <span className="font-bold">80</span> / 100
+              </div>
+            </div>
+            <div className="flex justify-between rounded-md bg-yellow-500/10 p-2 sm:p-4">
+              <div className="flex items-center gap-2 font-bold text-yellow-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 28 28"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M13 4c-1.91 0-3.457 1.387-3.844 3.188C6.824 7.604 5 9.55 5 12c0 .246.027.465.063.688C4.418 13.636 4 14.753 4 16c0 1.797.895 3.309 2.156 4.406c-.09.356-.156.711-.156 1.094c0 2.473 2.027 4.5 4.5 4.5h.063c.582 1.129 1.59 2 2.937 2a3.49 3.49 0 0 0 2.5-1.063A3.49 3.49 0 0 0 18.5 28c1.348 0 2.355-.871 2.938-2h.062c2.348 0 4.273-1.828 4.469-4.125H26c0-.063-.027-.125-.031-.188c.004-.062.031-.125.031-.187c0-.371-.133-.688-.219-1.031C27.078 19.367 28 17.829 28 16c0-1.246-.418-2.363-1.063-3.313c.036-.222.063-.44.063-.687c0-2.45-1.824-4.395-4.156-4.813C22.457 5.388 20.91 4 19 4c-1.2 0-2.266.559-3 1.406C15.266 4.56 14.2 4 13 4zm0 2c1.117 0 2 .883 2 2v2c0 1.668-1.332 3-3 3v2c1.133 0 2.16-.39 3-1.031V24.5c0 .84-.66 1.5-1.5 1.5a1.47 1.47 0 0 1-1.469-1.219l-.187-.968l-.969.156A2.487 2.487 0 0 1 8 21.5c0-.309.05-.598.156-.875c.008-.023.024-.04.031-.063a2.468 2.468 0 0 1 1.813-1.5l-.406-1.968A4.452 4.452 0 0 0 7 18.656A3.958 3.958 0 0 1 6 16a3.9 3.9 0 0 1 .875-2.469l.313-.375l-.094-.5A2.858 2.858 0 0 1 7 12c0-1.668 1.332-3 3-3h1V8c0-1.117.883-2 2-2zm6 0c1.117 0 2 .883 2 2v1h1c1.668 0 3 1.332 3 3c0 .215-.04.422-.094.656l-.093.5l.312.375A3.9 3.9 0 0 1 26 16c0 1.2-.512 2.27-1.344 3c-.683-.605-1.554-1-2.531-1c-.945 0-1.797.336-2.469.906l1.282 1.532A1.806 1.806 0 0 1 22.125 20c.965 0 1.734.723 1.844 1.656C23.887 22.973 22.836 24 21.5 24c-.09 0-.2-.004-.375-.031l-.969-.157l-.187.97A1.47 1.47 0 0 1 18.5 26c-.84 0-1.5-.66-1.5-1.5V13.969A4.915 4.915 0 0 0 20 15v-2c-1.668 0-3-1.332-3-3V8c0-1.117.883-2 2-2z"
+                  />
+                </svg>
+                Memory
+              </div>
+              <div>
+                <span className="font-bold">92</span> / 100
+              </div>
+            </div>
+            <div className="flex justify-between rounded-md bg-green-500/10 p-2 sm:p-4">
+              <div className="flex items-center gap-2 font-bold text-green-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 22 22"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 15q.325 0 .537-.213t.213-.537v-8.5q0-.325-.213-.537T12 5q-.325 0-.537.213t-.213.537v8.5q0 .325.213.537T12 15Zm-2.75-2q.325 0 .537-.213T10 12.25v-4.5q0-.325-.213-.537T9.25 7q-.325 0-.537.213T8.5 7.75v4.5q0 .325.213.537T9.25 13Zm-2.5-2q.325 0 .537-.213t.213-.537v-.5q0-.325-.213-.537T6.75 9q-.325 0-.537.213T6 9.75v.5q0 .325.213.537T6.75 11Zm8 2q.325 0 .537-.213t.213-.537v-4.5q0-.325-.213-.537T14.75 7q-.325 0-.537.213T14 7.75v4.5q0 .325.213.537t.537.213Zm2.5-2q.325 0 .537-.213T18 10.25v-.5q0-.325-.213-.537T17.25 9q-.325 0-.537.213t-.213.537v.5q0 .325.213.537t.537.213ZM2 19.575V4q0-.825.588-1.413T4 2h16q.825 0 1.413.588T22 4v12q0 .825-.588 1.413T20 18H6l-2.3 2.3q-.475.475-1.088.213T2 19.575Zm2-2.4L5.175 16H20V4H4v13.175ZM4 4v13.175V4Z"
+                  />
+                </svg>
+                Verbal
+              </div>
+              <div>
+                <span className="font-bold">61</span> / 100
+              </div>
+            </div>
+            <div className="flex justify-between rounded-md bg-violet-500/10 p-2 sm:p-4">
+              <div className="flex items-center gap-2 font-bold text-violet-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 6.5a9.77 9.77 0 0 1 8.82 5.5c-1.65 3.37-5.02 5.5-8.82 5.5S4.83 15.37 3.18 12A9.77 9.77 0 0 1 12 6.5m0-2C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zm0 5a2.5 2.5 0 0 1 0 5a2.5 2.5 0 0 1 0-5m0-2c-2.48 0-4.5 2.02-4.5 4.5s2.02 4.5 4.5 4.5s4.5-2.02 4.5-4.5s-2.02-4.5-4.5-4.5z"
+                  />
+                </svg>
+                Visual
+              </div>
+              <div>
+                <span className="font-bold">72</span> / 100
+              </div>
+            </div>
+          </div>
+          <button className="rounded-full bg-sky-700 p-2 text-white transition duration-75 hover:bg-sky-800 hover:shadow-md active:translate-y-0.5">
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import ResultSummary from '@/components/ResultSummary';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import ResultSummary from "@/components/ResultSummary";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-	return (
-		<main className="grid min-h-screen place-items-center">
-			<ResultSummary />
-		</main>
-	);
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <ResultSummary />
+    </main>
+  );
 }
