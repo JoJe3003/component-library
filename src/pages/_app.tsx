a0657b6717c8974823dcf1956d28988c4bc748c8
JoJe3003 <jfjespersen@gmail.com>
2023-04-18 21:50:58 +0200

    Added mobile dynamics to ResultSummary component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Noto_Sans } from 'next/font/google';
-
-const mainFont = Noto_Sans({
-	subsets: ['latin'],
-	weight: '400',
-});
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<main className={mainFont.className}>
-			<Component {...pageProps} />
-		</main>
-	);
+	return <Component {...pageProps} />;
 }
