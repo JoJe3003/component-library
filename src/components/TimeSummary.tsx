aadedf6835548dcc37dcd1f261ca1dd467bd4e37
JoJe3003 <jfjespersen@gmail.com>
2023-05-13 12:35:05 +0200

    Added better styling to TimeSummary component

--- src/components/TimeSummary.tsx
+++ src/components/TimeSummary.tsx
@@ -41,11 +41,12 @@ export default function TimeSummary() {
   };
 
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-4 scale-[2]">
       <textarea
+        id="input"
+        name="input"
         contentEditable={true}
         onInput={handleTextAreaInput}
-        className="w-24 overflow-hidden text-right"
         // make spaces in the input area into "\n"
         onKeyDown={(e) => {
           if (e.key === " ") {
@@ -53,22 +54,30 @@ export default function TimeSummary() {
             e.currentTarget.value += "\n";
           }
         }}
+        placeholder="Enter time.."
+        className="w-24 overflow-hidden rounded-sm border-0 py-1.5 text-right text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder-shown:h-8"
       />
-      <div className="relative mt-2">
+      <div className="my-1.5 overflow-hidden rounded-t-sm">
         {time.map((time, i) => (
           <div
             key={i}
-            className="text-right before:absolute before:left-0 before:content-['+'] first:before:content-none odd:bg-gray-200"
+            className="flex justify-end px-1 text-right odd:bg-gray-200"
           >
-            {time.hours}h {time.minutes}m
+            <span className={i == 0 ? `hidden` : `mr-auto`}>+</span>
+            <span className="ml-1 flex">
+              {time.hours}h<span className="w-9">{time.minutes}m</span>
+            </span>
           </div>
         ))}
 
         {time.length > 0 && (
           <div>
-            <hr className="border-t border-black" />
-            <div className="border-b-2 border-double border-black before:mr-1 before:content-['=']">
-              {sum.hours}h {sum.minutes}m
+            <hr className="border-t-1 border-black" />
+            <div className="flex justify-end border-b-4 border-double border-black px-1 text-right">
+              <span className="mr-auto">=</span>
+              <span className="ml-1 flex">
+                {sum.hours}h<span className="w-9">{sum.minutes}m</span>
+              </span>
             </div>
           </div>
         )}
