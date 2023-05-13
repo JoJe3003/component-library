7e853995e38e90a32dd27ac8a1157980d7c42e11
JoJe3003 <jfjespersen@gmail.com>
2023-05-13 17:21:06 +0200

    Improved code structure and design of TimeSummary

--- src/components/TimeSummary.tsx
+++ src/components/TimeSummary.tsx
@@ -36,96 +36,95 @@ export default function TimeSummary() {
       })
       .filter((time) => !isNaN(time.hours) && !isNaN(time.minutes));
     setTime(time);
+  };
 
-    // console log every object in the time array
+  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    // make spaces in the input area into "\n"
+    if (e.key === " ") {
+      e.preventDefault();
+      e.currentTarget.value += "\n";
+    }
+    // dont allow any inputs other than the list below
+    if (
+      !(
+        (e.key >= "0" && e.key <= "9") ||
+        e.key === "Backspace" ||
+        e.key === "Enter" ||
+        e.key === " " ||
+        e.key === "ArrowUp" ||
+        e.key === "ArrowDown" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowRight" ||
+        e.key === "Tab" ||
+        e.key === "Delete" ||
+        e.key === "Home" ||
+        e.key === "End" ||
+        e.key === "PageUp" ||
+        e.key === "PageDown" ||
+        e.ctrlKey === true
+      )
+    ) {
+      e.preventDefault();
+    }
+  };
+
+  const handleOnKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    // remove whitespace from the input area
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      e.currentTarget.value = e.currentTarget.value.replace(/\n\n/g, "\n");
+    }
+    // dont allow first entry to be "\n"
+    if (e.currentTarget.value === "\n") {
+      e.currentTarget.value = "";
+    }
   };
 
   return (
     <div>
-      <div className="flex scale-[2] gap-4">
-        <div className="relative">
+      <div className="flex items-stretch">
+        <div className="flex">
           <textarea
             id="input"
             name="input"
-            inputMode="numeric"
-            placeholder="Enter time.."
+            placeholder="Enter time..."
             onInput={handleTextAreaInput}
-            // make spaces in the input area into "\n"
-            onKeyDown={(e) => {
-              if (e.key === " ") {
-                e.preventDefault();
-                e.currentTarget.value += "\n";
-              }
-              // dont allow any other symbols than numbers and "\n"
-              if (
-                !(
-                  (e.key >= "0" && e.key <= "9") ||
-                  e.key === "Backspace" ||
-                  e.key === "Enter" ||
-                  e.key === " " ||
-                  e.key === "ArrowUp" ||
-                  e.key === "ArrowDown" ||
-                  e.key === "ArrowLeft" ||
-                  e.key === "ArrowRight" ||
-                  e.key === "Tab" ||
-                  e.key === "Delete" ||
-                  e.key === "Home" ||
-                  e.key === "End" ||
-                  e.key === "PageUp" ||
-                  e.key === "PageDown" ||
-                  e.ctrlKey === true
-                )
-              ) {
-                e.preventDefault();
-              }
-            }}
-            onKeyUp={(e) => {
-              if (e.key === "Enter" || e.key === " ") {
-                e.preventDefault();
-                e.currentTarget.value = e.currentTarget.value.replace(
-                  /\n\n/g,
-                  "\n"
-                );
-              }
-              // dont allow first entry to be "\n"
-              if (e.currentTarget.value === "\n") {
-                e.currentTarget.value = "";
-              }
-            }}
-            className="h-full w-24 resize-none overflow-hidden rounded-sm border-0 py-1.5 text-right text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-sm placeholder:text-gray-400 placeholder-shown:h-8 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 [&::-webkit-resizer]:hidden"
+            onKeyDown={handleOnKeyDown}
+            onKeyUp={handleOnKeyUp}
+            className="h-full w-32 resize-none overflow-hidden rounded-sm border-0 py-0 text-right text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-left placeholder:text-gray-400 placeholder-shown:h-9 placeholder-shown:w-32 placeholder-shown:py-1.5 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:leading-6 "
           />
-          {time.length > 0 && (
-            <span className="absolute -bottom-6 left-11 min-w-max text-right text-sm text-gray-400">
-              Rows: {time.length}
-            </span>
-          )}
         </div>
-        <div className="my-1.5 overflow-hidden rounded-t-sm">
-          {time.map((time, i) => (
-            <div
-              key={i}
-              className="flex justify-end px-1 text-right odd:bg-gray-200"
-            >
-              <span className={i == 0 ? `hidden` : `mr-auto`}>+</span>
-              <span className="ml-1 flex">
-                {time.hours}h<span className="w-9">{time.minutes}m</span>
-              </span>
-            </div>
-          ))}
+        {time.length > 0 && (
+          <div className="ml-4 overflow-hidden rounded-t-sm">
+            {time.map((time, i) => (
+              <div
+                key={i}
+                className="flex justify-end px-1 text-right odd:bg-gray-200"
+              >
+                <span className={i == 0 ? `hidden` : `mr-auto`}>+</span>
+                <span className="ml-1 flex">
+                  <span>{time.hours}h</span>
+                  <span className="w-9">{time.minutes}m</span>
+                </span>
+              </div>
+            ))}
 
-          {time.length > 0 && (
             <div>
               <hr className="border-t-1 border-black" />
               <div className="flex justify-end border-b-4 border-double border-black px-1 text-right">
                 <span className="mr-auto">=</span>
                 <span className="ml-1 flex">
-                  {sum.hours}h<span className="w-9">{sum.minutes}m</span>
+                  <span>{sum.hours}h</span>
+                  <span className="w-9">{sum.minutes}m</span>
                 </span>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
+      {time.length > 0 && (
+        <span className="text-xs text-gray-400">Rows: {time.length}</span>
+      )}
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import ResultSummary from "@/components/ResultSummary";
 import DarkToggle from "@/components/DarkToggle";
 import PaymentCard from "@/components/PaymentCard";
 import TimeSummary from "@/components/TimeSummary";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className="grid min-h-screen place-items-center">
-      {/* <ResultSummary /> */}
-      {/* <DarkToggle /> */}
-      {/* <PaymentCard /> */}
-      <TimeSummary />
+    <main className="grid min-h-screen place-items-center bg-slate-100">
+      <div className="rounded-md border bg-white p-4">
+        {/* <ResultSummary /> */}
+        {/* <DarkToggle /> */}
+        {/* <PaymentCard /> */}
+        <TimeSummary />
+      </div>
     </main>
   );
 }
